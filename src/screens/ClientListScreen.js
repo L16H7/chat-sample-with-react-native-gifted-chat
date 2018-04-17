e1a4fa2dc4ee23469243f8a5f4a6604780c1e982
lightMK <linmyatko@hotmail.com>
2018-04-17 17:16:03 +0630

    client list rendered

--- src/screens/ClientListScreen.js
+++ src/screens/ClientListScreen.js
@@ -14,52 +14,33 @@ class ClientListScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      agents: []
+      clients: []
     };
 
-     const clients = [
-      {
-        name: '...' 
-      }
-    ];
-   
-    this.createDataSource(clients);
+    this.clientData = [];
+    _.map(this.props.navigation.state.params, (e) => {
+      this.clientData.push(e);
+    });
+    this.createDataSource(this.clientData);
   }
 
   componentDidMount() {
-    console.log('>>ClientListScreen Mounted');
-    console.log(this.props);
   }
-
-  componentWillReceiveProps(nextProps) {
-    this.agentData = [];
-    _.map(nextProps.agents, (e) => {
-      this.agentData.push(e);
-    });
-    this.createDataSource(this.agentData);
-  }
-
-  /*
-  static getDerivedStateFromProps(nextProps, prevState) {
-    this.agentData = [];
-    _.map(nextProps.agents, (e) => {
-      this.agentData.push(e);
-    });
-    console.log(this.agentData);
-    this.createDataSource(this.agentData);
-    return nextProps;
-  }
-  */
 
   createDataSource(agents) {
     const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.dataSource = ds.cloneWithRows(agents);
   }
 
+  _agentList() {
+    console.log('back???');
+    this.props.navigation.navigate('Agents');
+  }
+
   render() {
     return (
       <View style={{ flex: 1, flexDirection: "column" }}>
-        <Navbar title={'Clients'} button={''} action={() => this._friendList()} />
+        <Navbar title={'Clients'} button={'Back'} action={() => this._agentList()} />
         <ListView
           enableEmptySections
           dataSource={this.dataSource}
