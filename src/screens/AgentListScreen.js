6ac4738a0cca4f6e6288fe0da648c84cf312e009
L16H7 <linmyatko@hotmail.com>
2018-04-17 13:33:44 +0630

    ListView added

--- src/screens/AgentListScreen.js
+++ src/screens/AgentListScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, ListView } from "react-native";
 import Navbar from "../components/NavBar";
 import AgentList from '../components/agents/AgentList';
 
@@ -15,21 +15,48 @@ class AgentListScreen extends Component {
     this.state = {
       agents: []
     };
+
+     const agents = [
+      {
+        name: "name1"
+      },
+      {
+        name: 'name2'
+      },
+      {
+        name: 'name3'
+      }
+    ];
+   
+    this.createDataSource(agents);
   }
 
   componentDidMount() {
     this.props.getAgents("company-0001");
   }
 
-  static getDerivedStateFromProps(nextProps, prevState) {
+  getDerivedStateFromProps(nextProps, prevState) {
     console.log('WRP');
     console.log(nextProps);
+    // this.createDataSource(nextProps.agents);
+    return nextProps;
+  }
+
+  createDataSource(agents) {
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.dataSource = ds.cloneWithRows(agents);
   }
 
   render() {
     return (
       <View style={{ flex: 1, flexDirection: "column" }}>
         <Navbar title={"Login"} button={""} action={() => this._friendList()} />
+        <ListView
+          enableEmptySections
+          // contentContainerStyle={styles.grid}
+          dataSource={this.dataSource}
+          renderRow={(e) => <Text>{e.name}</Text> }
+        />
       </View>
     );
   }
